99bcc3beaf05aa137f7a0acd2da0454b8c15bd80
bkorumaz <bahadir.korumaz@gmail.com>
2020-05-07 15:37:15 +0300

    created indexpage with articles using bootstrap

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ const articleRouter = require('./routes/articles');
 app.set('view engine', 'ejs'); //view engine will convert ejs code to html
 
 app.use('/articles', articleRouter)
-
+const articles = [{
+          title: 'Test Article',
+          createdAt: new Date(),
+          description: 'Text description'
+},
+{
+          title: 'Test Article 2',
+          createdAt: new Date(),
+          description: 'Text description 2'
+}]
 app.get("/", (req, res) => {
-  res.render('index'); //render acess view folder
+  res.render('index', { articles : articles}); //render acess view folder
 });
 
 app.listen(5001);
