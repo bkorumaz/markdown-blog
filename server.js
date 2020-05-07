e95233a0ba056a0aa786b4f828df82c485e1979b
bkorumaz <bahadir.korumaz@gmail.com>
2020-05-07 15:59:37 +0300

    added new article page

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const articleRouter = require('./routes/articles');
+const articleRouter = require("./routes/articles");
 
-app.set('view engine', 'ejs'); //view engine will convert ejs code to html
+app.set("view engine", "ejs"); //view engine will convert ejs code to html
 
-app.use('/articles', articleRouter)
-const articles = [{
-          title: 'Test Article',
-          createdAt: new Date(),
-          description: 'Text description'
-},
-{
-          title: 'Test Article 2',
-          createdAt: new Date(),
-          description: 'Text description 2'
-}]
+app.use("/articles", articleRouter);
+const articles = [
+  {
+    title: "Test Article",
+    createdAt: new Date(),
+    description: "Text description",
+  },
+  {
+    title: "Test Article 2",
+    createdAt: new Date(),
+    description: "Text description 2",
+  },
+];
 app.get("/", (req, res) => {
-  res.render('index', { articles : articles}); //render acess view folder
+  res.render("articles/index", { articles: articles }); //render acess view folder
 });
 
 app.listen(5001);
