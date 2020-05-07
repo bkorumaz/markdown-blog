4c30237d72e6661a1bf88e5ecdbe94b1edae6da8
bkorumaz <bahadir.korumaz@gmail.com>
2020-05-07 08:07:10 +0300

    added frst most basic route

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const articleRouter = require('./routes/articles');
 
 app.set('view engine', 'ejs'); //view engine will convert ejs code to html
 
+app.use('/articles', articleRouter)
+
 app.get("/", (req, res) => {
-  res.render('index');
+  res.render('index'); //render acess view folder
 });
 
 app.listen(5001);
